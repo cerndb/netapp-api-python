717bcbda15c6e64b7911da24504dcf3cb67104f8
Albin Stjerna <albin.stjerna@cern.ch>
2016-09-07 11:28:17 +0200

    Add some tests exercising the API.

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 1. `mkvirtualenv monitoring-experiment`
 2. `workon monitoring-experiment`
 3. `pip install -r requirements.txt`
+
+
+## Testing
+
+Set the environment variables `NETAPP_HOST`, `NETAPP_USERNAME`, and
+`NETAPP_PASSWORD` and run `pytest`.
--- netapp/tests/test_api.py
+++ netapp/tests/test_api.py
@@ -5,13 +5,32 @@ import netapp.api
 import pytest
 
 server_host = os.environ['NETAPP_HOST']
-server_port = os.environ['NETAPP_PORT']
 server_username = os.environ['NETAPP_USERNAME']
 server_password = os.environ['NETAPP_PASSWORD']
 
 
+def _connect_server():
+    return netapp.api.Server(hostname=server_host, username=server_username,
+                             password=server_password)
+
+
 def test_list_all_events():
-    pass
+    s = _connect_server()
+
+    found_anything = False
+    for event in s.events:
+        found_anything = True
+        assert event
+
+    assert found_anything
 
 def test_list_events_after():
-    pass
+
+    s = _connect_server()
+
+    found_anything = False
+    for event in s.events.greater_than_id(0):
+        found_anything = True
+        assert event
+
+    assert found_anything
