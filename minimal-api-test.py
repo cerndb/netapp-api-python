e03ccf3c4678c23735cd07b82913a0fc609faa48
Albin Stjerna <albin.stjerna@cern.ch>
2016-09-09 17:02:50 +0200

    Add a minimal API exercise function (WIP)

---
+++ minimal-api-test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2.7
+
+import os
+
+import netapp.api
+from netapp import vocabulary as xml
+#import netapp.vocalbulary
+
+import lxml.etree
+
+OCUM_API_URL = '/apis/XMLrequest'
+
+XMLNS = 'http://www.netapp.com/filer/admin'
+XMLNS_VERSION = "1.0"
+
+if __name__ == '__main__':
+    netapp.api._DEBUG = True
+    server_host = os.environ['NETAPP_HOST']
+    server_username = os.environ['NETAPP_USERNAME']
+    server_password = os.environ['NETAPP_PASSWORD']
+
+    s = netapp.api.Server(hostname=server_host, username=server_username,
+                         password=server_password)
+
+    event_iter = xml.event_iter(
+        xml.timeout("4"),
+        xml.greater_than_id("999")
+    )
+
+    s.perform_call(event_iter)
