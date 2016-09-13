c2e88c81bac1e85da57cd89f7953935bd2c3397b
Albin Stjerna <albin.stjerna@cern.ch>
2016-09-13 13:58:15 +0200

    Typo: other.

--- netapp/api.py
+++ netapp/api.py
@@ -351,7 +351,7 @@ class Event(object):
 
     def __eq__(self, other):
         return(isinstance(other, self.__class__)
-               and self.__dict__ == oter.__dict__)
+               and self.__dict__ == other.__dict__)
 
     def __ne__(self, other):
         return not self.__eq__(other)
