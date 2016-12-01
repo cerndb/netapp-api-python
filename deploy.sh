489ad439817dc9efaa67ce80dc6d333da0dbd459
Albin Stjerna <albin.stjerna@cern.ch>
2016-12-01 12:58:01 +0100

    Try a slightly leaner approach to doc deploy

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,14 @@ set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
-DOCDIR="docs"
-DOC_BUILD_DIR="$DOCDIR/_build/html"
-START_DIR=$(pwd)
 
-function doCompile {
-    make html
-}
+TARGET=$1
+
+pwd
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompile
+    echo "Skipping deploy"
     exit 0
 fi
 
@@ -25,19 +21,12 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO $DOC_BUILD_DIR
-cd $DOC_BUILD_DIR
+git clone $REPO $TARGET
+
+cd $TARGET
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd $START_DIR/$DOCDIR
-
-# Clean out existing contents
-rm -rf $START_DIR/$DOC_BUILD_DIR/**/* || exit 0
-
-# Run our compile script
-doCompile
 
 # Now let's go have some fun with the cloned repo
-cd $START_DIR/$DOC_BUILD_DIR
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
@@ -49,18 +38,18 @@ fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
+# git add .
+# git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_github_netapp.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
 
 # Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+#git push $SSH_REPO $TARGET_BRANCH
