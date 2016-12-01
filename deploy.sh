d311019d33df4c777d9e85b3f700ff3ad149986a
Albin Stjerna <albin.stjerna@cern.ch>
2016-12-01 14:01:53 +0100

    More verbosity, use separate repo dir

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
+set -v
+set -x
 
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 TARGET=$1
+PAGES_REPO_PATH="deploy_repo"
+
+rm -rf $PAGES_REPO_PATH
 
 pwd
 
@@ -21,14 +26,16 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO $TARGET
+git clone $REPO $PAGES_REPO_DIR
 
-cd $TARGET
+cd $PAGES_REPO_DIR
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 
 # Now let's go have some fun with the cloned repo
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+rsync -a ../$TARGET/ $PAGES_REPO_PATH/
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if [ -z `git diff --exit-code` ]; then
@@ -42,7 +49,7 @@ fi
 # git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
- ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
